3b08b27664a033429f1997eb81a3c9b72cd637ef
chancho808 <kyc222@naver.com>
2026-06-20 12:06:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,14 +20,3 @@ def health():
     return {
         "status": "healthy"
     }
-
-# =========================
-# MAIN RUN (Render 안정용)
-# =========================
-if __name__ == "__main__":
-    import os
-    import uvicorn
-
-    port = int(os.environ.get("PORT", 10000))
-
-    uvicorn.run(app, host="0.0.0.0", port=port)
