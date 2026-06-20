9e815f5437a6afeb36cf5f18413dbbf37b84129a
chancho808 <kyc222@naver.com>
2026-06-20 11:22:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,5 +25,9 @@ def health():
 # MAIN RUN (Render 안정용)
 # =========================
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    port = int(os.environ.get("PORT", 10000))
+
+    uvicorn.run(app, host="0.0.0.0", port=port)
